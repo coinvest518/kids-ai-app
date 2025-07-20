72d4ff2f5782ec46ce45459b569943d645dfd472
promise Me <coinvest518@gmail.com>
2025-07-19 20:44:45 -0400

    net

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,11 +1,14 @@
 import { Client, Account, Databases, ID } from "appwrite";
 
 
-const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
 
-const account = new Account(client);
-const databases = new Databases(client);
+let client, account, databases;
+if (typeof window !== 'undefined') {
+  client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+  account = new Account(client);
+  databases = new Databases(client);
+}
 
 export { client, account, databases, ID };
